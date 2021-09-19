b8c29e81867f3c8f8eea7df83bf92db3211849f2
weijchen0919 <weijchen0919@gmail.com>
2021-09-19 19:50:47 -0400

    add player animation

--- Assets/Scripts/PeopleAnimation.cs
+++ Assets/Scripts/PeopleAnimation.cs
@@ -4,33 +4,24 @@ using UnityEngine;
 
 public class PeopleAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Animator peopleAni;
+    private Animator animator;
 
     void Start()
     {
-        peopleAni = this.gameObject.GetComponent<Animator>();
-        //peopleAni.applyRootMotion = false;
-        peopleAni.Play("Running");
-        //peopleAni.SetBool("OnGround", true);
-
-        //Debug.Log(peopleAni);
+        animator = gameObject.GetComponent<Animator>();
+        animator.Play("Running");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Debug.Log(this.gameObject.transform.position.y);
-        if (this.gameObject.transform.position.y > 2  )
+        if (gameObject.transform.position.y > 2)
         {
-            peopleAni.SetBool("OnGround", false);
-            
-            //Debug.Log("false");
+            animator.SetBool("OnGround", false);
         }
-        if (this.gameObject.transform.position.y <= 2)
+        
+        if (gameObject.transform.position.y <= 2)
         {
-            peopleAni.SetBool("OnGround", true);
-            //Debug.Log("true");
+            animator.SetBool("OnGround", true);
         }
     }
 }
---
+++ Assets/Scripts/PlayerB.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerB : MonoBehaviour
+{
+    private PlayerManager _playerManager;
+    private Vector3 BornVector = Vector3.zero;
+    private bool isSaved = false;
+
+    [SerializeField] float destroyTime = 20.0f;
+
+    void Start()
+    {
+        _playerManager = FindObjectOfType<PlayerManager>();
+        BornVector = new Vector3(0, Random.Range(-8.0f, 8.0f), 0);
+        Invoke("DestroyPrefab", destroyTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "Hook")
+        {
+            isSaved = true;
+            _playerManager.AddSavedAmount();
+        }
+    }
+
+    public Vector3 GetBornVector()
+    {
+        return BornVector;
+    }
+    
+    private void DestroyPrefab()
+    {
+        if (!isSaved)
+        {
+            _playerManager.AddDeadAmount();
+        }        
+        Destroy(gameObject);
+    }
+} 
