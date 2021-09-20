8981f8e6d7e7283e13ec1dffe144ce0c6a19ce9d
weijchen0919 <weijchen0919@gmail.com>
2021-09-19 21:24:36 -0400

    finish end canvas

--- Assets/EndCanvas.cs
+++ Assets/EndCanvas.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndCanvas : MonoBehaviour
 {
@@ -26,5 +27,15 @@ public class EndCanvas : MonoBehaviour
         comboScoreText.text = _gameManager.GetComboScore().ToString();
         finalScoreText.text = _gameManager.GetFinalScore().ToString();
         rankText.text = _gameManager.GetRank();
+    }
+
+    public void RestartButtonOnClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitButtonOnClick()
+    {
+        Application.Quit();
     }
 }
